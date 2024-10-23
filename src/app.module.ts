7090274b75c9a49ad51d42ba4448069ad850fccb
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-10-23 16:43:29 -0500

    init config

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { NatsModule } from './transports/nats.module';
+import { AuthModule } from './auth/auth.module';
+@Module({
+  imports: [NatsModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('OrdersMS-Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers
+      },
+    },
+  );
+  await app.listen();
+  logger.log(`Microservice running on port ${envs.appPort}`);
+}
+bootstrap();
